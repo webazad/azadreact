b64d968794296a26ccf9ae8007d14f94514d872c
Azad <azad@azad.com>
2018-11-04 12:36:34 +0600

    Main Page

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,31 +2,35 @@
  * and open the template in the editor.
  */
 import React, {Component} from "react";
+import Background from './pattern-green.jpg';
+
 class Main extends Component{
     render(){
         return(
-            <div className="main">
-                <section className="">
-                    <h2>Main Section</h2> 
-                    <p>Section Main Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good. </p>
-                </section>
-                <section className="main_bottom">
+            <section className="main_section" style={{backgroundImage:"url("+Background+")"}}>
+                <div className="main_upper">
+                    <div className="upper_box">
+                        <h2>Special Features</h2> 
+                        <p>Section Main Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. </p>
+                    </div>    
+                </div>
+                <div className="main_lower">
                     <div className="box">
                         <div className="colm_3">
-                            <h3>One</h3> 
-                            <p>Section Main Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do.</p>
+                            <h3>Web Design</h3> 
+                            <p>Section Main Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence.</p>
                         </div>
                         <div className="colm_3">
-                            <h3>Two</h3> 
-                            <p>Section Main Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed </p>
+                            <h3>Web Developement</h3> 
+                            <p>Section Main Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence.</p>
                         </div>
-                        <div className="col_3">
-                            <h3>Three</h3> 
-                            <p>Section Main Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betr</p>
+                        <div className="colm_3">
+                            <h3>Apps Development</h3>
+                            <p>Section Main Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence.</p>
                         </div>
                     </div>
-                </section>
-            </div>                
+                </div>
+            </section>                
         );
     }
 }
--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -3,6 +3,9 @@
  */
 import React, {Component} from "react";
 class Portfolio extends Component{
+    constructor(props){
+        super();
+    }
     render(){
         return(
             <div className="">
@@ -12,32 +15,32 @@ class Portfolio extends Component{
                 </section>
                 <section style={{ 'background' : "#e3e3e3",'padding' : "20px 0px" }}>
                     <h2>Demos of ...</h2>
-                    <ul style={{ 'display' : "inline-block" }}>
-                        <li><a href="https://www.templatemonster.com/demo/54875.html" title="Demo 1" target="_blank">Demo 1</a></li>
-                        <li><a href="http://preview.themeforest.net/item/little-people-kindergarten-wordpress-theme-for-prescool-and-infants-nurseries-and-play-school/full_screen_preview/11494908?ref=athemez&clickthrough_id=1389647478&redirect_back=true" target="_blank" title="Demo 2">Demo 2</a></li>
-                        <li><a href="http://preview.themeforest.net/item/fable-children-kindergarten-wordpress-theme/full_screen_preview/9294431?ref=athemez&clickthrough_id&redirect_back=true" target="_blank" title="Demo 3">Demo 3</a></li>
+                    <ul>
+                        <li><a href='https://www.templatemonster.com/demo/54875.html' rel="noopener noreferrer" target="_blank">Demo 1</a></li>
+                        <li><a href="http://preview.themeforest.net/item/little-people-kindergarten-wordpress-theme-for-prescool-and-infants-nurseries-and-play-school/full_screen_preview/11494908?ref=athemez&clickthrough_id=1389647478&redirect_back=true" rel="noopener noreferrer" target="_blank" title="Demo 2">Demo 2</a></li>
+                        <li><a href="http://preview.themeforest.net/item/fable-children-kindergarten-wordpress-theme/full_screen_preview/9294431?ref=athemez&clickthrough_id&redirect_back=true" target="_blank" title="Demo 3" rel="noopener noreferrer">Demo 3</a></li>
                         <li><a href="https://webthemez.com/preview/?fit-club-gym-fitness-free-website-template" target="Demo 4" title="Demo 4">Demo 4</a></li>
                         <li><a href="http://preview.themeforest.net/item/arans-kindergarten-school-html-template/full_screen_preview/22585282?_ga=2.97788191.1685481296.1540993583-1977898323.1540993583&_gac=1.248785525.1540993850.CjwKCAjwpeXeBRA6EiwAyoJPKsUcYsYHYVa1Kdmkv4MSrjyXG4EC-pzjBCykd-B3rZqyzw1tlILY2xoCee0QAvD_BwE" target="Demo 5" title="Demo 5">Demo 5</a></li>
-                        <li><a href="https://blackrockdigital.github.io/startbootstrap-creative/" target="_blank" title="Demo 6">Demo 6</a></li>
-                        <li><a href="https://webthemez.com/preview/?blush-beauty-salon-responsive-website-template-free-download" target="_blank" title="Demo 7">Demo 7</a></li>
-                        <li><a href="https://webthemez.com/preview/?swift-car-wash-html-website-template" target="_blank" title="Demo 8">Demo 8</a></li>
-                        <li><a href="https://webthemez.com/preview/?xtream-corporate-responsive-website-template" target="_blank" title="Demo 9">Demo 9</a></li>
+                        <li><a href="https://blackrockdigital.github.io/startbootstrap-creative/" rel="noopener noreferrer" target="_blank" title="Demo 6">Demo 6</a></li>
+                        <li><a href="https://webthemez.com/preview/?blush-beauty-salon-responsive-website-template-free-download" rel="noopener noreferrer" target="_blank" title="Demo 7">Demo 7</a></li>
+                        <li><a href="https://webthemez.com/preview/?swift-car-wash-html-website-template" rel="noopener noreferrer" target="_blank" title="Demo 8">Demo 8</a></li>
+                        <li><a href="https://webthemez.com/preview/?xtream-corporate-responsive-website-template" rel="noopener noreferrer" target="_blank" title="Demo 9">Demo 9</a></li>
                     </ul>
                 </section>
                 <section style={{ 'background' : "#ffffff",'padding' : "20px 0px" }}>
                     <h2>Completed sites ...</h2>
-                    <ol style={{ 'display' : "inline-block" }}>
-                        <li><a href="http://www.jcurvemedia.com.au/" target="_blank" title="J-Curve Media">J-Curve Media</a></li>
-                        <li><a href="https://tinkerbellhomecleaning.com/" target="_blank" title="Tinker Bell Home Cleaning">Tinker Bell Home Cleaning</a></li>
-                        <li><a href="http://www.permanentbeautytherapy.com/" target="_blank" title="Permanent Beauty Therapy">Permanent Beauty Therapy</a></li>
-                        <li><a href="http://celluloidsisters.com/" target="_blank" title="Celluloid Sisters">Celluloid Sisters</a></li>
-                        <li><a href="http://exchangeed.com/" target="_blank" title="Exchangeed">Exchangeed</a></li>
-                        <li><a href="http://social.exchangeed.com/" target="_blank" title="social.exchangeed">social.exchangeed</a></li>
-                        <li><a href="https://fantasyhall.net/" target="_blank" title="Fantasy Hall">Fantasy Hall</a></li>
-                        <li><a href="https://fantasyhall.com/" target="_blank" title="Fantasy Hall">Fantasy Hall</a></li>
-                        <li><a href="http://www.capcitykitchen.com/" target="_blank" title="Fantasy Hall">Capital City Kitchen</a></li>
-                        <li><a href="https://austin4u.com/" target="_blank" title="Austin">Austin</a></li>
-                        <li><a href="#" target="_blank" title="Fantasy Hall">Name</a></li>
+                    <ol>
+                        <li><a href="http://www.jcurvemedia.com.au/" rel="noopener noreferrer" target="_blank" title="J-Curve Media">J-Curve Media</a></li>
+                        <li><a href="https://tinkerbellhomecleaning.com/" rel="noopener noreferrer" target="_blank" title="Tinker Bell Home Cleaning">Tinker Bell Home Cleaning</a></li>
+                        <li><a href="http://www.permanentbeautytherapy.com/" rel="noopener noreferrer" target="_blank" title="Permanent Beauty Therapy">Permanent Beauty Therapy</a></li>
+                        <li><a href="http://celluloidsisters.com/" rel="noopener noreferrer" target="_blank" title="Celluloid Sisters">Celluloid Sisters</a></li>
+                        <li><a href="http://exchangeed.com/" rel="noopener noreferrer" target="_blank" title="Exchangeed">Exchangeed</a></li>
+                        <li><a href="http://social.exchangeed.com/" rel="noopener noreferrer" target="_blank" title="social.exchangeed">social.exchangeed</a></li>
+                        <li><a href="https://fantasyhall.net/" rel="noopener noreferrer" target="_blank" title="Fantasy Hall">Fantasy Hall</a></li>
+                        <li><a href="https://fantasyhall.com/" rel="noopener noreferrer" target="_blank" title="Fantasy Hall">Fantasy Hall</a></li>
+                        <li><a href="http://www.capcitykitchen.com/" rel="noopener noreferrer" target="_blank" title="Fantasy Hall">Capital City Kitchen</a></li>
+                        <li><a href="https://austin4u.com/" rel="noopener noreferrer" target="_blank" title="Austin">Austin</a></li>
+                        <li><a href="" target="_blank" rel="noopener noreferrer" title="Fantasy Hall">Awaiting...</a></li>
                     </ol>
                 </section>
             </div>                
